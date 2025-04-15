9b8e870903f1e6e97b75155bc0fbc3dad59874d3
Sudhish <149787916+Sudhish69@users.noreply.github.com>
2025-04-15 14:49:24 +0000

    config changes

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+let accountId=12345
+const accountEmail="sudhish2gmial.com"
+var accountPassword="sexi"
+accountCity="Kolkata"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+accountId=897
+accountPassword="unable"
+console.table([accountId,accountEmail,accountPassword,accountCity])
