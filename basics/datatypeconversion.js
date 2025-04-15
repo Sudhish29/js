8ed292ec6034cb1c908ef672ae9e72539075e874
Sudhish <149787916+Sudhish69@users.noreply.github.com>
2025-04-15 23:40:58 +0000

    datatype conversions

---
+++ basics/datatypeconversion.js
@@ -0,0 +1,17 @@
+let score="   "
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Boolean(score);
+console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+/*"33"=>33
+"33abc"=>NaN
+true=1 false =0*/
+let n=29
+let stringnumber=String(n)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
--- basics/variables.js
+++ basics/variables.js
@@ -1,9 +1,9 @@
 let accountId=12345
-const accountEmail="sudhish2gmial.com"
+const accountEmail="sudhish@gmail.com"
 var accountPassword="sexi"
 accountCity="Kolkata"
 console.table([accountId,accountEmail,accountPassword,accountCity])
 accountId=897
 accountPassword="unable"
-let name
-console.table([accountId,accountEmail,accountPassword,accountCity,name])
+/*let name*/
+console.table([accountId,accountEmail,accountPassword,accountCity])
