a63152800088c1924c18576164cf90b97ac5ec1e
Sudhish <149787916+Sudhish69@users.noreply.github.com>
2025-04-16 15:42:56 +0000

    your commit message

---
+++ basics/datatypeConversion.js
@@ -0,0 +1,28 @@
+let score="   "
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Boolean(score);
+// console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+/*"33"=>33
+"33abc"=>NaN
+true=1 false =0*/
+let n=29
+let stringnumber=String(n)
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+//Operations
+let value=3
+let negvalue=-value
+// console.log(negvalue);
+// console.log(true);
+// console.log(+true);
+// console.log("1"+2)
+// console.log(1+"2")
+console.log("1"+4+5)//145
+console.log(1+5+"4"+1+5+"29")//641529    //result will depend if the string operations come before or after the operation if string preceedes then whole answer will be converted to string
+
+
+
