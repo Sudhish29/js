1b36b78266747c8df935a0b17e70d3af554e0230
Sudhish <149787916+Sudhish69@users.noreply.github.com>
2025-04-29 19:32:52 +0000

    changes

---
+++ basics/datatypecomparison.js
@@ -0,0 +1,15 @@
+// console.log("2">1)
+// console.log("02">1);//automatically converting string to integer to give the valid result
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);//equality and comparisons work differently as comparisons convert null to 0 whereas equality does not change null to 0
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);//undefined will always give false in all the operations
+
+console.log(2===2);
+console.log("2"===2);
+
+
+
+
