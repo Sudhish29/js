58ae8cdbbdc365f496120dbec5f10d7c5bf4fc81
Sudhish <149787916+Sudhish69@users.noreply.github.com>
2025-04-30 15:21:28 +0000

    string operations

--- basics/strings.js
+++ basics/strings.js
@@ -1,3 +1,20 @@
 let name="Sudhish"
 let usn="1ms23is128"
 console.log(`My name is ${name} and usn is ${usn}`);
+const gameName=new String('Sudhish')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.charAt(1));
+console.log(gameName.indexOf('d'));
+const newString=gameName.substring(0,4)//negative indices are considered as 0
+console.log(newString);
+const anotherString=gameName.slice(-2,2)//charascters are given from the last position
+console.log(anotherString);
+const hi='My name is Sudhish Mazumdar'
+console.log(hi);
+
+// console.log(hi.trim());
+console.log(hi.split(' '));
+
+
+
