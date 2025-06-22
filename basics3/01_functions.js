1bc5cb758aa4778691d73f4beb9bbb28fafe5767
Sudhish <149787916+Sudhish29@users.noreply.github.com>
2025-06-22 20:20:11 +0000

    basics3/01_functions.js

--- basics3/01_functions.js
+++ basics3/01_functions.js
@@ -19,7 +19,7 @@ function addTwoNumbers(number1,number2){
     
 }
 const result=addTwoNumbers(9,5)
-console.log("Result:"+result);
+// console.log("Result:"+result);
 
 
 function loginUserMessage(username="s"){   //default value is passed if nothing is passed s will be printrd instead of undefined
@@ -30,6 +30,33 @@ function loginUserMessage(username="s"){   //default value is passed if nothing 
     }
     return `${username} just logged in`
 }
-// console.log(loginUserMessage());    //undefined
+//  console.log(loginUserMessage());    //undefined
 
-console.log(loginUserMessage("Sudhish")); 
+// console.log(loginUserMessage("Sudhish")); 
+function calculateCartPrice(val1,val2,val3, ...num){        //... is rest operator and it contains all the values in a single array
+    //the val1,val2 and val3 takes the first 3 values and the rest are contained in the array by the..,(rest opertor) 
+   return num
+    
+}
+ console.log(calculateCartPrice(200,100,500));
+ const user={
+    username:"Sudhish",
+    price:69
+  
+ }
+ function handleObject(hello){
+   console.log(`Username is ${hello.username} and price is ${hello.price} `);
+   }
+// handleObject(user)
+handleObject({
+    username:"Rand",
+    price:6969,
+    cartcolor:"red"
+})
+const arr=[100,200,300,400]
+function returnSecondValue(get){
+    return get[0]
+}
+// console.log(returnSecondValue(arr));
+console.log(returnSecondValue([50,100,150,200]));
+
