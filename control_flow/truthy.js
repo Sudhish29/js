8c4b6004b24dce9ba61216f427c79a58b7f03b45
Sudhish <149787916+Sudhish29@users.noreply.github.com>
2025-08-27 10:38:05 +0000

    truthy values

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -19,8 +19,9 @@ if (userEmail.length === 0) {
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
+if(Object.keys(emptyObj).length===0){
+    console.log("Object is Empty");
+    
 }
 
 // Nullish Coalescing Operator (??): null undefined
