9a6e646ff16ca10d7e0052598c88bacd0de3e130
Sudhish <149787916+Sudhish29@users.noreply.github.com>
2025-08-27 17:39:54 +0000

    for each callback function

---
+++ iterations/five.js
@@ -0,0 +1,46 @@
+const arr=["France","Switzerland","Austria","Italy"]
+arr.forEach((a,b,c)=>{
+console.log(a,b,c);
+
+})
+    
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item,i) => {
+    if(i==0){
+    console.log(item.languageName);
+    }
+} )
