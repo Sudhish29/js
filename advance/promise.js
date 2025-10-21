cdddceaba8479b34a03b45f2f73a715b46122a62
Sudhish <149787916+Sudhish29@users.noreply.github.com>
2025-10-21 13:51:11 +0000

    promise function

--- advance/promise.js
+++ advance/promise.js
@@ -13,8 +13,39 @@ new Promise(function(resolve,reject){
     setTimeout(function(){
     console.log("Async is completed");
     resolve()
-    },1000)
+    },2000)
 }).then(function(){
     console.log("Promise is completed");
     
 })
+const promiseThree=new Promise(function(resolve,reject){
+    resolve({username:'Sudhish',email:'sudhish@msrit.com'})
+})
+promiseThree.then(function(user){
+    console.log(user);
+    
+})
+const promiseFour=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=false;
+        if(!error){
+            resolve({username:'Sudhish',pass:'123'})
+        }
+        else{
+            reject("Error occured")
+        }
+        
+    },1000)
+})
+promiseFour.then((user)=>{
+    console.log(user.username);
+    
+}).then((username)=>{
+    console.log(username);
+    
+}).catch((error)=>{
+    console.log(error);
+    
+}).finally(()=>{console.log("Promise is either resolved or rejected");
+}
+)
